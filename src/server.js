1ecec7ef22f35f22efc30f0aaf2ce61adeb4fba2
Animesh3547 <asofficial3547@gmail.com>
2026-03-24 22:57:51 +0530

    Refactor server initialization with async setup
    Refactor server startup to ensure database connection before starting the server and sensor simulation.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
 import { connectDB } from "./config/db.js";
-
 import healthRoutes from "./routes/healthRoutes.js";
 import sensorRoutes from "./routes/sensorRoutes.js";
 import systemRoutes from "./routes/systemRoutes.js";
@@ -13,9 +11,6 @@ dotenv.config();
 
 const app = express();
 
-connectDB();
-startSensorSimulation();
-
 app.use(cors());
 app.use(express.json());
 
@@ -23,8 +18,27 @@ app.use("/api/health", healthRoutes);
 app.use("/api/sensors", sensorRoutes);
 app.use("/api/system", systemRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Backend running 🚀");
+});
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,"0.0.0.0" ,  () => {
-  console.log(`Server running on port ${PORT}`);
-});
+/* 🔥 IMPORTANT FIX */
+const startServer = async () => {
+  try {
+    await connectDB();   // wait for DB
+
+    startSensorSimulation();  // start AFTER DB
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error("Startup error:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
